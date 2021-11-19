ca5b1be64250e247a6dc8deaa047a4a201c7dc93
Chen Jie <chenjie@Chens-MacBook-Pro.local>
2021-11-20 04:15:13 +0800

    Render table and add css styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,112 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components';
 
 import { useUsersBalanceQuery } from './apis';
 
-function App() {
+export const App: FC = () => {
   const { data, isLoading, error } = useUsersBalanceQuery();
 
-  console.log(data);
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  const renderedResults = data?.map((userBalance, idx) => {
+    return (
+      <tr key={`userBalance.user-${idx}`}>
+        <td>{userBalance.user}</td>
+        <td>{userBalance.BTC ? `${userBalance.BTC} BTC` : <>&mdash;</>}</td>
+        <td>{userBalance.ETH ? `${userBalance.ETH} ETH` : <>&mdash;</>}</td>
+      </tr>
+    );
+  });
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Wrapper>
+        <Table>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>BTC Amount</th>
+              <th>ETH Amount</th>
+            </tr>
+          </thead>
+          <tbody>{renderedResults}</tbody>
+        </Table>
+      </Wrapper>
+    </Container>
   );
+};
+
+const Container = styled.div`
+  display: relative;
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  width: 80%;
+  height: 80%;
+  margin: auto;
+`;
+
+const Table = styled.table`
+  border-collapse: collapse;
+  background-color: white;
+  width: 100%;
+  text-align: center;
+  color: hsl(208, 96%, 57%);
+  margin-bottom: 50px;
+
+  thead > tr {
+    height: 40px;
+    font-size: 24px;
+    color: hsl(208, 96%, 57%);
+    background-color: hsl(212, 40%, 20%);
+    cursor: pointer;
+  }
+
+  tbody tr {
+    height: 60px;
+    font-size: 20px;
+    padding: 16px 24px;
+    border-bottom: 3px solid hsl(200, 100%, 64%);
+    :hover {
+      background-color: lightblue;
+    }
+  }
+`;
+
+const Spinner = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid grey;
+  border-radius: 50%;
+  border-top-color:black;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
 }
+
+@keyframes spin {
+  to { -webkit-transform: rotate(360deg); }
+}
+@-webkit-keyframes spin {
+  to { -webkit-transform: rotate(360deg); }
+
+`;
 
 export default App;
