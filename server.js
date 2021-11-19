59696fb637bdcc5b402205d0bed5a3d9f633300f
Chen Jie <chenjie@Chens-MacBook-Pro.local>
2021-11-19 22:06:49 +0800

    Add logic to return totalAssetValue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,14 +17,43 @@ const userBalances = {
   },
 };
 
-app.get('/api/v1/users/:id', (req, res) => {
+async function tickerQuery(ticker) {
+  const res = await fetch(`https://www.bitstamp.net/api/v2/ticker/${ticker}`);
+
+  return res.json();
+}
+
+async function calculateTotalValue(userBalance) {
+  let totalAssetValue = 0;
+  const BTCPrice = await tickerQuery('BTCUSD');
+  const ETHPrice = await tickerQuery('ETHUSD');
+
+  for (const key in userBalance) {
+    if (key === 'BTC') {
+      totalAssetValue += userBalance[key] * BTCPrice.last;
+    }
+    totalAssetValue += userBalance[key] * ETHPrice.last;
+  }
+
+  return totalAssetValue;
+}
+
+app.get('/api/v1/users/:id', async (req, res) => {
   const key = `user-${req.params.id}`;
-  const userBalance = { [key]: userBalances[key] };
+  const userBalance = userBalances[key];
 
   if (!userBalances[key]) {
-    res.status(404).send('The customer with the given ID was not found');
+    res.status(404).send(`The user with the ID ${req.params.id} was not found`);
   }
-  res.json(userBalance);
+
+  try {
+    const totalAssetValue = await calculateTotalValue(userBalance);
+    res.send(
+      `The total asset value for ${key} is $${totalAssetValue.toFixed(2)} USD`
+    );
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.get('/api/v1/users', (req, res) => {
